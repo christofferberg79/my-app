4f5a2979ffe77d18b9f3684ad7f97faff18bd8df
Christoffer Berg <christofferberg79@gmail.com>
2019-01-02 15:18:26 +0100

    log visits in db

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
     kotlin("jvm") version "1.3.11"
     application
     id("com.github.johnrengelman.shadow") version "4.0.3"
+    id("com.github.ben-manes.versions") version "0.20.0"
 }
 
 group = "cberg"
@@ -12,15 +14,19 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     jcenter()
+    maven("https://dl.bintray.com/kotlin/ktor")
 }
 
 val ktorVersion = "1.1.1"
 val logbackVersion = "1.2.3"
+val exposedVersion = "0.11.2"
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("org.jetbrains.exposed:exposed:$exposedVersion")
+    implementation("org.postgresql:postgresql:42.2.5")
 
     testImplementation(kotlin("test"))
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
@@ -45,4 +51,19 @@ tasks.create("stage") {
 tasks.withType<ShadowJar> {
     classifier = ""
     version = ""
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview")
+                    .map { Regex(".*[.-]$it[.\\d-]*", RegexOption.IGNORE_CASE) }
+                    .any { candidate.version.matches(it) }
+                if (rejected) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
 }
--- src/main/kotlin/cberg/myapp/MyApp.kt
+++ src/main/kotlin/cberg/myapp/MyApp.kt
@@ -2,18 +2,45 @@ package cberg.myapp
 
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
+
+object Visits : Table("visit") {
+    val visitedAt = datetime("visited_at")
+}
+
+data class Visit(val visitedAt: DateTime)
 
 fun Application.main() {
+    Database.connect(System.getenv("JDBC_DATABASE_URL"), driver = "org.postgresql.Driver")
+
     routing {
         get("/") {
             call.respondText("OK")
         }
 
         get("/db") {
-            call.respondText(System.getenv("DATABASE_URL"))
+            transaction {
+                Visits.insert { stmt ->
+                    stmt[visitedAt] = DateTime.now()
+                }
+            }
+            val res = transaction {
+                val num = Visits.visitedAt.count().alias("as num")
+                val last = Visits.visitedAt.max().alias("as last")
+                Visits.slice(num, last)
+                    .selectAll()
+                    .map { rs -> rs[num] to rs[last] }.firstOrNull()
+            }
+            check(res != null)
+            call.respondText("Number of visits: ${res.first}\nLast visit: ${res.second}")
+//            call.respondText(System.getenv("DATABASE_URL"))
         }
     }
 }
