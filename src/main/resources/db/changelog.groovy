74ad1a31c889926fab3f3968364a35ff88c117bb
Christoffer Berg <christofferberg79@gmail.com>
2019-04-18 08:53:33 +0200

    cleaned up db changelog

--- src/main/resources/db/changelog.groovy
+++ src/main/resources/db/changelog.groovy
@@ -2,14 +2,6 @@ package db
 
 databaseChangeLog {
     changeSet(id: "1", author: "cberg") {
-        createTable(tableName: "visit") {
-            column(name: "visited_at", type: "timestamp") {
-                constraints(nullable: false)
-            }
-        }
-    }
-
-    changeSet(id: "2", author: "cberg") {
         createTable(tableName: "todo") {
             column(name: "id", type: "uuid") {
                 constraints(primaryKey: true)
